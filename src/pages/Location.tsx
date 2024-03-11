67a043f32059bc246139054b89e58d97343b18c7
philippbruhin <info@philippbruhin.ch>
2024-03-11 11:26:07 +0100

    add button quiz to location

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,4 +1,5 @@
-import Maps from "../components/Maps/Maps";
+import Maps from '../components/Maps/Maps';
+import { Link } from 'react-router-dom';
 
 export default function Location() {
   return (
@@ -15,6 +16,14 @@ export default function Location() {
       <div className="not-prose">
         <Maps />
       </div>
+      <p className="py-4 text-center pt-10">
+        <Link
+          className="rounded-md bg-blue-600 px-10 py-4 text-white hover:text-white no-underline shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          to="/wasserrad/quiz"
+        >
+          Quiz starten
+        </Link>
+      </p>
     </div>
   );
 }
