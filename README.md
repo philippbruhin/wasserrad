428137e06ce692ebd287cb5b8335a2402e620c31
philippbruhin <info@philippbruhin.ch>
2024-08-27 21:04:57 +0200

    limit storage integration results from 1'008 to 1'000

--- README.md
+++ README.md
@@ -289,6 +289,8 @@ Zusätzlich muss man einen API-Key erstellen, damit die Daten vom TTN Portal abg
 
 Die Sensoreinstellungen können (durch Administratoren von diesem Projekt) unter [waterwheel.console.cloud.thethings.industries](https://waterwheel.console.cloud.thethings.industries/) aktualisiert werden.
 
+> Es scheint, dass das Limit für die Anzahl der Einträge Ende August 2024 auf 1'000 Einträge reduziert wurde. Ursprünglich haben wir Abfragen für 7 Tage x 24 Stunden x 6 (alle 10 Minuten) = 1'008 Werte durchgeführt. Jetzt können wir jedoch nur noch 1'000 Einträge abfragen. Leider wird in der Dokumentation der [Storage Integration API](https://www.thethingsindustries.com/docs/api/reference/grpc/storage_integration/) beim Feld `limit` keine Begrenzung erwähnt.
+
 ### 3.3 Empfänger des Sensorsignals (Gateway)
 
 Das vom am Wasserrad montierten Sensor versendete Signal wird gleich von zwei Gateways (anderes Wort für Empfängerantenne) empfangen. Die eine Antenne befindet sich im Leuholz, die andere in der Nähe des Fussballplatzes in Siebnen.
--- src/lib/ttnDataFetcher.ts
+++ src/lib/ttnDataFetcher.ts
@@ -1,70 +1,72 @@
-import axios from 'axios';
+import axios from "axios";
 
 export type SensorData = {
-  entries:        Entry[];
-}
+  entries: Entry[];
+};
 
 export type Entry = {
-  result:         Result;
-}
+  result: Result;
+};
 
 export type Result = {
   end_device_ids: EndDeviceIDS;
-  received_at:    Date;
+  received_at: Date;
   uplink_message: UplinkMessage;
-}
+};
 
 export type EndDeviceIDS = {
   device_id: string;
-}
+};
 
 export type UplinkMessage = {
   decoded_payload: DecodedPayload;
-  received_at:     Date;
-}
+  received_at: Date;
+};
 
 export type DecodedPayload = {
-  ADC_CH0V:        number;
-  BatV:            number;
-  Count:           number;
+  ADC_CH0V: number;
+  BatV: number;
+  Count: number;
   Digital_IStatus: string;
-  TempC1:          string;
-  Work_mode:       string;
-}
+  TempC1: string;
+  Work_mode: string;
+};
 
 export async function ttnDataFetcher() {
   try {
-    const type = 'uplink_normalized';
+    const type = "uplink_normalized";
     const response = await axios.get(
       `${import.meta.env.VITE_TTN_API_URL}/api/v3/as/applications/` +
-      `${import.meta.env.VITE_TTN_APP_ID}/devices/` + 
-      `${import.meta.env.VITE_TTN_DEVICE_ID}/packages/storage/${type}`, {
+        `${import.meta.env.VITE_TTN_APP_ID}/devices/` +
+        `${import.meta.env.VITE_TTN_DEVICE_ID}/packages/storage/${type}`,
+      {
         headers: {
-          'Authorization': `Bearer ${import.meta.env.VITE_TTN_API_KEY}`,
-          'Accept': 'text/event-stream',
+          Authorization: `Bearer ${import.meta.env.VITE_TTN_API_KEY}`,
+          Accept: "text/event-stream",
         },
         params: {
-          'f_port': 2,
-          'order': '-received_at',
-          'field_mask': 'up.uplink_message.decoded_payload',
-          'limit': '1008',
-        }
-    });
+          f_port: 2,
+          order: "-received_at",
+          field_mask: "up.uplink_message.decoded_payload",
+          limit: 1000, // Should be 1008 for 7 days of data but TTN only allows 1000.
+        },
+      }
+    );
 
     const jsonStringsArray: string[] = response.data
       .trim()
-      .split('\n\n') // Assuming each JSON object is on a new line
-      .reverse();    // Reverse the since fetch is -received_at
+      .split("\n\n") // Assuming each JSON object is on a new line
+      .reverse(); // Reverse the since fetch is -received_at
 
     const sensorData: SensorData = {
-      entries: []
+      entries: [],
     };
-    
+
     jsonStringsArray.forEach((jsonString: string) => {
       try {
         const jsonObject: Result = JSON.parse(jsonString).result;
         const sensorDataEntry: Entry = {
-          result: jsonObject
+          result: jsonObject,
         };
         sensorData.entries.push(sensorDataEntry);
       } catch (error) {
