8c7434f602d679ab56b6aa682005b01027044d63
philippbruhin <info@philippbruhin.ch>
2024-09-01 01:24:27 +0200

    add counter data from august

--- src/lib/waterwheelMonthlyData.ts
+++ src/lib/waterwheelMonthlyData.ts
@@ -8,7 +8,7 @@ export const waterwheelMonthlyData: WaterwheelMonthlyData[] = [
   { value: 44300, date: new Date(2024, 4, 31) }, // May 2024
   { value: 162508, date: new Date(2024, 5, 30) }, // June 2024
   { value: 145429, date: new Date(2024, 6, 31) }, // July 2024
-  { value: 0, date: new Date(2024, 7, 31) }, // August 2024
+  { value: 60399, date: new Date(2024, 7, 31) }, // August 2024
   { value: 0, date: new Date(2024, 8, 30) }, // September 2024
   { value: 0, date: new Date(2024, 9, 31) }, // October 2024
   { value: 0, date: new Date(2024, 10, 30) }, // November 2024
